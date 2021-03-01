2858b0b50aaff7bba091ab390936c87a6d1ff809
EternalEffy <39722892+EternalEffy@users.noreply.github.com>
2021-03-01 18:34:52 +0300

    иммитация работы логгера

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ public class Main {
     public static void main(String[] args) {
     EFFY_Logger myLogger = new EFFY_Logger();
     myLogger.print("системный сбой");
-    myLogger.write("log.txt", true);
+    for(int i=0;i<10;i++) {
+        myLogger.write("log.txt", true);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     }
 }
